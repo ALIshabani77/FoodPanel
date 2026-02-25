bee71fe848307446e87b14b674dd65101077778b
Ali Shabani <alishabaniara123@gmail.com>
2026-02-25 21:36:46 +0330

    chore(core): update django project configuration and main urls

--- food_panel/urls.py
+++ food_panel/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
+# food_panel/urls.py
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('dashboard.urls')),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # این خط باید api خالی باشد تا با فرانت‌اِند ست شود
+    path('api/', include('dashboard.urls')), 
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
